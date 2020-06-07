05e60851424581d95db475f1f7dae036020f7bdf
pjdidelot <pjdidelot@gmail.com>
2020-06-07 11:46:45 -0400

    C

---
+++ Assets/LScript/member/IMember.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LScript {
+
+    public interface IMember {
+
+        /// <summary>
+        /// Evaluates the member to an integer.
+        /// </summary>
+        int evalInt();
+
+        /// <summary>
+        /// Evaluates the member to a string.
+        /// </summary>
+        string evalString();
+    }
+}
