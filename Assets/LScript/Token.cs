d6eba5cc49642f0213ee87410c20a2ede11a575d
pjdidelot <pjdidelot@gmail.com>
2020-06-07 11:46:58 -0400

    C

---
+++ Assets/LScript/Token.cs
@@ -0,0 +1,17 @@
+﻿namespace LScript {
+
+    public struct Token {
+
+        public readonly BrickColor first;
+        public readonly BrickColor second;
+
+        public Token(BrickColor char1, BrickColor char2) {
+            this.first = char1;
+            this.second = char2;
+        }
+
+        public override string ToString() {
+            return "TOKEN(" + this.first.ToString() + ", " + this.second.ToString() + ")";
+        }
+    }
+}
